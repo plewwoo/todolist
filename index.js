ef8773b2cee4fc0adcd6886872e5521581bc4a25
theplew14412 <pew_woothiphom@hotmail.com>
2021-08-27 17:47:59 +0700

    new feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('express-handlebars');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index')
 const apiRouter = require('./routes/api');
@@ -13,13 +14,14 @@ app.set('view engine', 'hbs');
 
 //Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
+app.use(express.json()); //เช็คว่าเป็น json
 app.use(express.urlencoded({ extended: false }));
 
 //Routes
+app.use(cors())
 app.use('/', indexRouter)
 app.use('/tasks/', apiRouter)
 
-app.listen(3000, () => {
-    console.log('listening to port 3000')
+app.listen(3001, () => {
+    console.log('listening to port 3001')
 });
--- routes/api.js
+++ routes/api.js
@@ -1,50 +1,60 @@
 const express = require('express');
 const router = express.Router();
 
-let data = require('../data/data')
-
 const dataJson = require('../data/todolist.json')
 const obj = JSON.stringify(dataJson)
 const jsObj = JSON.parse(obj)
 
-let currentTaskId = Object.keys(jsObj.task).length;
-console.log(currentTaskId)
+let currentTaskId = Object.keys(jsObj.task).length
+// let currentTaskId = 0
+
 
 router.get('/', (req, res) => {
     res.json(jsObj);
 });
 
-router.get('/:id', (req, res) => {
-    const tasksId = Number.parseInt(req.params.id);
-    const task = jsObj.task.find((task) => task.id === tasksId)
-    res.json(task);
-});
+// router.get('/:id', (req, res) => {
+//     const tasksId = Number.parseInt(req.params.id);
+//     const task = jsObj.task.find((task) => task.id === tasksId)
+//     res.json(task);
+// });
 
-router.post('/', (req, res) => {
-    currentTaskId += 1;
+router.post('/post', (req, res) => {
+    currentTaskId += 1
+    console.log('post id :', currentTaskId)
+    const title = req.body.title
+
     const newTask = {
         id: currentTaskId,
-        ...req.body
+        title: title
     };
     jsObj.task.push(newTask);
     res.json(newTask);
 });
 
-router.patch('/:id', (req, res) => {
-    const tasksId = Number.parseInt(req.params.id);
+router.put('/update', (req, res) => {
+    // const tasksId = Number.parseInt(req.params.id);
+    // const taskIndex = jsObj.task.findIndex((task) => task.id === tasksId);
+
+    const tasksId = req.body.id
+    console.log('put id :', tasksId)
     const taskIndex = jsObj.task.findIndex((task) => task.id === tasksId);
+    const title = req.body.title
+
     const updatedTask = {
         id: tasksId,
-        ...req.body
+        title: title
     };
     jsObj.task[taskIndex] = updatedTask;
     res.json(updatedTask);
+    console.log(updatedTask)
 });
 
-router.delete('/:id', (req, res) => {
-    currentTaskId -= 1;
+router.delete('/delete/:id', (req, res) => {
     const tasksId = Number.parseInt(req.params.id);
-    const taskIndex = jsObj.task.findIndex((task) => task.id === tasksId)
+    console.log('delete id :', tasksId)
+    const taskIndex = jsObj.task.findIndex((task) => task.id === tasksId);
+    console.log('taskIndex:', taskIndex)
     jsObj.task.splice(taskIndex, 1);
     res.sendStatus(200)
 });
